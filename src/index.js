f30ebfba3a06388b2ea25a8dd269d012d23b37fc
Dmitry Malashev <dmalashev@gmail.com>
2024-10-15 11:53:27 +0300

    feat: solve the reverse int task

--- src/index.js
+++ src/index.js
@@ -1,3 +1,10 @@
 module.exports = function reverse (n) {
-  
+  const str = String(Math.abs(n));
+  let newStr = '';
+
+  for (let i = 0; i < str.length; i++) {
+    newStr = str[i] + newStr;
+  }
+
+  return Number(newStr);
 }
